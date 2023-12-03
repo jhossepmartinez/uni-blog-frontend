255ca35f625867fd2c38b7f7550a630ff21fb26f
jhossepmartinez <jhossepmv@gmail.com>
2023-12-03 20:37:15 -0300

    done header styling and responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
 
                     <div className="lg:hidden items-center justify-end">
                         <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={nav_visibility_handler}>
-                            <span class="sr-only">Open menu</span>
+                            <span className="sr-only">Open menu</span>
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
@@ -38,27 +38,28 @@ export default function Header() {
                     </div>
                 </nav>
 
+
                 {nav_is_visible && (
-                    <div class="lg:hidden" role="dialog" aria-modal="true">
-                        <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-4 py-5 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                            <div class="flex items-center justify-end">
-                                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={nav_visibility_handler}>
-                                    <span class="sr-only">Close menu</span>
-                                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    <div className="lg:hidden" role="dialog" aria-modal="true">
+                        <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-4 py-5 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                            <div className="flex items-center justify-end">
+                                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={nav_visibility_handler}>
+                                    <span className="sr-only">Close menu</span>
+                                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 </button>
                             </div>
 
-                            <div class="mt-6 flow-root">
-                                <div class="-my-6 divide-y divide-gray-500/10">
-                                    <div class="space-y-2 py-6">
-                                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
-                                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
-                                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+                            <div className="mt-6 flow-root">
+                                <div className="-my-6 divide-y divide-gray-500/10">
+                                    <div className="space-y-2 py-6">
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Comunidad</a>
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Universidades</a>
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Realidades</a>
                                     </div>
-                                    <div class="py-6">
-                                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+                                    <div className="py-6">
+                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Iniciar Sesion</a>
                                     </div>
                                 </div>
                             </div>
