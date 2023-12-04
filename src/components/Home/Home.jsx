bdf72f4dbe8348ec9ba96ab538752e0045b3a5f7
jhossepmartinez <jhossepmv@gmail.com>
2023-12-03 22:01:07 -0300

    navigation filter is a component now

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Card from '../Card/Card'
+import NavigationFilter from '../NavigationFilter/NavigationFilter'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <div className="pl-4 pb-2 pt-10">
+                <NavigationFilter />
+            </div>
+            <div>
+                <Card />
+            </div>
+
+        </div>
     )
 }
---
+++ src/components/NavigationFilter/NavigationFilter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavigationFilter() {
+    return (
+        <div className="flex gap-x-8">
+            <button>
+                Nuevo
+            </button>
+            <button>
+                Popular
+            </button>
+        </div>
+    )
+}
