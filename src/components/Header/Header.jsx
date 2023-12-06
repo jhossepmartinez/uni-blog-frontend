5628b5c9b78f8df2747b28d3253630bb3e647972
jhossepmartinez <jhossepmv@gmail.com>
2023-12-05 23:51:25 -0300

    added transition to sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,33 +39,29 @@ export default function Header() {
                 </nav>
 
 
-                {nav_is_visible && (
-                    <div className="lg:hidden" role="dialog" aria-modal="true">
-                        <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-4 py-5 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                            <div className="flex items-center justify-end">
-                                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={nav_visibility_handler}>
-                                    <span className="sr-only">Close menu</span>
-                                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                    </svg>
-                                </button>
-                            </div>
+                    <div className={`transition-all duration-200 ease-linear fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-4 py-5 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 ${!nav_is_visible ? 'opacity-0' : 'opacity-100'}`}>
+                        <div className="flex items-center justify-end">
+                            <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={nav_visibility_handler}>
+                                <span className="sr-only">Close menu</span>
+                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
 
-                            <div className="mt-6 flow-root">
-                                <div className="-my-6 divide-y divide-gray-500/10">
-                                    <div className="space-y-2 py-6">
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Comunidad</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Universidades</a>
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Realidades</a>
-                                    </div>
-                                    <div className="py-6">
-                                        <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Iniciar Sesion</a>
-                                    </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Comunidad</a>
+                                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Universidades</a>
+                                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Realidades</a>
+                                </div>
+                                <div className="py-6">
+                                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Iniciar Sesion</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                )}
 
             </header>
         </div>
