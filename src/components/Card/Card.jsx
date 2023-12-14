decfb73f121f9f594a729c94c0a6e763a84011bd
jhossepmartinez <jhossepmv@gmail.com>
2023-12-14 17:59:53 -0300

    now data from posts can be passed to Cards component and displayed at home page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({ title, summary, image}) {
     return (
         <div>
-            <a href="#" className="flex h-36 flex-row items-center bg-white border-t border-b">
-                <div className="flex flex-col max-h-[150px] pl-4 justify-between pr-[20px]">
-                    <h5 className="mb-2 font-bold text-gray-900">Mi experiencia realizando 3 ayudantias en la USM</h5>
-                    <p className="hidden mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                </div>
+            <a href="#" className="flex flex-row justify-between mx-4 gap-6 border-t-2 pt-10 pb-10">
                 <div className="">
-                    <img className="pr-4 max-w-[150px] max-h-[150px]" src="https://plus.unsplash.com/premium_photo-1667673941713-ad4d4751c93b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHVwcHklMjBkb2d8ZW58MHx8MHx8fDA%3D" alt="Dog" />
+                    <h5 className="mb-2 font-bold text-gray-900">{title}</h5>
+                    <p className="hidden mb-3 font-normal text-gray-700">{summary}</p>
+                </div>
+                <div className="flex">
+                    <img className="max-h-[100px] max-w-[150px]" src={image} alt="Dog" />
                 </div>
             </a>
         </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import Card from '../Card/Card'
 import NavigationFilter from '../NavigationFilter/NavigationFilter'
 
+import { getPosts } from '../../requests/Posts';
+
 export default function Home() {
+    const { data, error } = getPosts("http://localhost:3000/api/posts");
     return (
         <div>
             <div className="pl-4 pb-2 pt-10">
                 <NavigationFilter />
             </div>
             <div>
-                <Card />
+                {data.map((post) => (
+                    <Card
+                        key={post.id}
+                        title={post.title}
+                        summary={post.summary}
+                        image={`https://picsum.photos/id/${Math.floor(Math.random() * (1000 - 1)) + 1}/150/100`}
+                    />
+                ))}
             </div>
-
         </div>
     )
 }
