ef8df07e53de639911b83d5cb17c59a56c857ac1
jhossepmartinez <jhossepmv@gmail.com>
2023-12-14 23:56:46 -0300

    made create post function for frontend, momentary setup

--- src/requests/Posts.jsx
+++ src/requests/Posts.jsx
@@ -5,6 +5,7 @@ function getPosts(url) {
     const [error, setError] = useState([]);
 
     useEffect(() => {
+        console.log("effect getposts running!");
         const fetchData = async () => {
             try {
                 const response = await fetch(url);
@@ -17,11 +18,26 @@ function getPosts(url) {
         fetchData();
     }, [url]);
     
-    return { data, error };
+    return { data, error }
 }
 
-function createPost(url) {
-    return "xd"
+function createPost(url, newPost) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newPost)
+            });
+
+            const result = await response.json();
+            resolve(result);
+        } catch (error) {
+            reject(error);
+        }
+    })
 }
 
-export { getPosts }
+export { getPosts, createPost }
