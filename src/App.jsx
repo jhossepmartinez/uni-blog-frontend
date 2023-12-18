389b0b15da05a3dec1223c36840a7a421104491c
jhossepmartinez <jhossepmv@gmail.com>
2023-12-17 23:59:48 -0300

    added login router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Login from "./components/Login/Login"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
+    const [showHeader, setShowHeader] = useState(false);
+    useEffect(() => {
+        const currentPath = window.location.pathname;
+        setShowHeader(currentPath === "/");
+    })
     return (
-        <div>
-            <Header />
-            <div className="flex justify-center">
-                <Home />
-            </div>
-        </div>
+        <Router>
+            {showHeader && <Header />}
+            <Routes>
+                <Route path="/login" element={<Login />}>
+                </Route>
+                <Route path="/" element={<Home />}>
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
