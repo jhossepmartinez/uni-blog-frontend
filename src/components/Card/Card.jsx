49c99fdc743b01c8246fe05fc04d3d9ef9424bde
jhossepmartinez <jhossepmv@gmail.com>
2023-12-17 23:58:49 -0300

    fixed some styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card({ title, summary, image}) {
     return (
         <div>
-            <a href="#" className="flex flex-row justify-between mx-4 gap-6 border-t-2 pt-10 pb-10">
+            <a href="#" className="flex flex-row justify-between sm:w-[430px] md:w-[430px] mx-4 gap-6 border-t-2 pt-10 pb-10">
                 <div className="">
                     <h5 className="mb-2 font-bold text-gray-900">{title}</h5>
                     <p className="hidden mb-3 font-normal text-gray-700">{summary}</p>
